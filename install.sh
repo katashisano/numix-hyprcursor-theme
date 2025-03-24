8d0ceed9a44978f6359cd155a0e2943f268a4dc3
katashisano <itzidarrags0385@gmail.com>
2025-03-24 18:13:50 -0500

    Fix hotspot position

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 hyprcursor-util --create ./src/dark
 hyprcursor-util --create ./src/light
 
+rm -r ~/.local/share/icons/Numix-Cursor
+rm -r ~/.local/share/icons/Numix-Cursor-Light
 mv ./src/theme_Numix-Cursor ~/.local/share/icons/Numix-Cursor
 mv ./src/theme_Numix-Cursor-Light ~/.local/share/icons/Numix-Cursor-Light
 
